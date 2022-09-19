e9bdca71f14ba837782194afea7dae4d7640f5f9
gihants <gihan@macuject.com>
2022-09-20 00:23:07 +1000

    added extract videos frames from a directory

--- README.md
+++ README.md
@@ -1,37 +1,46 @@
 # extract_video_frames
-This is a simple opencv-python script to extract images from video frames:
+This is a simple opencv-python script to:
+1. extract frames from a video file (*.mp4)
+2. extract frames from a set of videos (*.mp4) in a directory
 
-## Requirements:
-- python 3+
-- OpenCV 3+
-
-## usage
-- clone the repository: 
+### usage
+1. clone the repository: 
     ```sh
     $ git clone https://github.com/gihants/extract_video_frames.git
     ```
-- run the python script with the required arguments:
 
-       -- argument "-i", "--input": path to input video file (required)
+2. install the requirements: 
+    ```sh
+    $ pip install -r requirements.txt
+    ```
+3. run the cli's with the required arguments:
+    1. extract frames from a single *.mp4 file:
+        ```sh
+        $ python ./extract_video_frames.py extract_single_video --video_file [video_file] --output_folder [output_folder]--skip [skip] --skip_beg [skip_beg] --frames [frames]
+        ```
 
-       -- argument "-o", "--output": name of the output folder (required)
-       
-       -- argument "-s", "--skip": number of initial frames to skip in between to control the frames per second to extract (optional)
+       Options:
 
-       -- argument "-sb", "--skip_beginning": number of initial frames to skip (optional)
-
-       -- argument "-f", "--frames": number of frames to extract (optional)
-       
-       
-
-    example #1: 
+            --video_file TEXT     Path of the video file to extract frames from
+            --output_folder TEXT  Path of the output directory to extract frames into
+            --skip INTEGER        Number of frames to skip / frames per sampling point
+            --skip_beg INTEGER    Number of frames to skip at the begining of the video
+            --frames INTEGER      Total number of frames to extract from the video
     
-    ```sh
-     $ python extract_video_frames.py -i VID_20200826_110451.mp4 -o extracted_directory 
-    ```
-    example #2: 
-    ```sh
-     $ python extract_video_frames.py -i VID_20200826_110451.mp4 -o extracted_directory -s 10 -f 100
-    ```
+    2. extract frames from all the *.mp4 files within a directory:
+        ```sh
+        $ python ./extract_video_frames.py extract_video_directory --video_directory [video_directory] --output_folder [output_folder]--skip [skip] --skip_beg [skip_beg] --frames [frames]
+        ```
+
+       Options:
+
+            --video_directory TEXT  Path of the directory containing video files to extract frames from
+            --output_folder TEXT  Path of the output directory to extract frames into
+            --skip INTEGER        Number of frames to skip / frames per sampling point
+            --skip_beg INTEGER    Number of frames to skip at the begining of the video
+            --frames INTEGER      Total number of frames to extract from the video
+
+       
+    
 
 
--- extract_video_frames.py
+++ extract_video_frames.py
@@ -1,53 +1,167 @@
-import argparse
-import cv2
+import glob
 import os
-    
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", required=True, type=str, help="path to input video file")
-ap.add_argument("-o", "--output", required=True, type=str, help="output directory name")
-ap.add_argument("-s", "--skip", default = 0, type=int, help="numberframes to skip in between")
-ap.add_argument("-sb", "--skip_beginning", default = -1, type=int, help="number of initial frames to skip")
-ap.add_argument("-f", "--frames", default = -1, type=int, help="number of frames to skip")
+from multiprocessing import Pool, cpu_count
+from typing import Dict
 
-args = vars(ap.parse_args())
-
-video_file = args["input"]
-output_folder = "./" + args["output"]
-skip = args["skip"]
-skip_beg = args["skip_beginning"]
-frames = args["frames"]
+import click
+import cv2
 
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-    
-vidcap = cv2.VideoCapture(video_file)
-success,image = vidcap.read()
-count = 0
-skipping_beginnng = 0
-frame_instance = 0
-while success:
-    if (frames >= 0) and (count > frames + skip):
-        break
-    if skipping_beginnng < skip_beg:
-        break
-    if frame_instance == skip:
-        frame_instance = 0
-        if count < 10:
-            cv2.imwrite(output_folder + "/frame000%d.jpg" % count, image)     # save frame as JPEG file       
-        elif count < 100:
-            cv2.imwrite(output_folder + "/frame00%d.jpg" % count, image)     # save frame as JPEG file
-        elif count < 1000:
-            cv2.imwrite(output_folder + "/frame0%d.jpg" % count, image)     # save frame as JPEG file
+def extract_frames_single_video(
+    video_file: str, output_folder: str, skip: int, skip_beg: int, frames: int
+):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    vidcap = cv2.VideoCapture(video_file)
+    success, image = vidcap.read()
+    count = 0
+    skipping_beginnng = 0
+    frame_instance = 0
+    while success:
+        if (frames >= 0) and (count > frames + skip):
+            break
+        if skipping_beginnng < skip_beg:
+            break
+        if frame_instance == skip:
+            frame_instance = 0
+            video_file_name = os.path.splitext(os.path.basename(video_file))[0]
+            file_name = f"{output_folder}/{video_file_name}_frame{count:05d}.jpg"
+            # file_name = f"{output_folder}/test.jpg"
+            cv2.imwrite(file_name, image)
         else:
-            cv2.imwrite(output_folder + "/frame%d.jpg" % count, image)     # save frame as JPEG file
-    else:
-         frame_instance += 1
-        
-                
-        
-    success,image = vidcap.read()
-    print('Extracted frame {}'.format(str(count)))
-    count += 1
-    skipping_beginnng += 1
-   
+            frame_instance += 1
+
+        success, image = vidcap.read()
+        print("Extracted frame {}".format(str(count)))
+        count += 1
+        skipping_beginnng += 1
+
+
+def extract_frames_single_video_worker(extract_params: Dict):
+    video_file = extract_params["video_file"]
+    output_folder = extract_params["output_folder"]
+    skip = extract_params["skip"]
+    skip_beg = extract_params["skip_beg"]
+    frames = extract_params["frames"]
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    vidcap = cv2.VideoCapture(video_file)
+    success, image = vidcap.read()
+    count = 0
+    skipping_beginnng = 0
+    frame_instance = 0
+    while success:
+        if (frames >= 0) and (count > frames + skip):
+            break
+        if skipping_beginnng < skip_beg:
+            break
+        if frame_instance == skip:
+            frame_instance = 0
+            video_file_name = os.path.splitext(os.path.basename(video_file))[0]
+            file_name = f"{output_folder}/{video_file_name}_frame{count:05d}.jpg"
+            # file_name = f"{output_folder}/test.jpg"
+            cv2.imwrite(file_name, image)
+        else:
+            frame_instance += 1
+
+        success, image = vidcap.read()
+        print("Extracted frame {}".format(str(count)))
+        count += 1
+        skipping_beginnng += 1
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name="extract_single_video")
+@click.option(
+    "--video_file", type=str, help="Path of the video file to extract frames from"
+)
+@click.option(
+    "--output_folder",
+    type=str,
+    help="Path of the output directory to extract frames into",
+)
+@click.option(
+    "--skip",
+    type=int,
+    default=10,
+    help="Number of frames to skip / frames per sampling point",
+)
+@click.option(
+    "--skip_beg",
+    type=int,
+    default=-1,
+    help="Number of frames to skip at the begining of the video",
+)
+@click.option(
+    "--frames",
+    type=int,
+    default=-1,
+    help="Total number of frames to extract from the video",
+)
+def extract_single_video(
+    video_file: str, output_folder: str, skip: int, skip_beg: int, frames: int
+):
+    extract_frames_single_video(video_file, output_folder, skip, skip_beg, frames)
+
+
+@cli.command(name="extract_video_directory")
+@click.option(
+    "--video_directory",
+    type=str,
+    help="Path of the directory containing video files to extract frames from",
+)
+@click.option(
+    "--output_folder",
+    type=str,
+    help="Path of the output directory to extract frames into",
+)
+@click.option(
+    "--skip",
+    type=int,
+    default=10,
+    help="Number of frames to skip / frames per sampling point",
+)
+@click.option(
+    "--skip_beg",
+    type=int,
+    default=-1,
+    help="Number of frames to skip at the begining of the video",
+)
+@click.option(
+    "--frames",
+    type=int,
+    default=-1,
+    help="Total number of frames to extract from the video",
+)
+def extract_video_directory(
+    video_directory: str, output_folder: str, skip: int, skip_beg: int, frames: int
+):
+    video_files = glob.glob(f"{video_directory}/*.mp4")
+    extract_params = [
+        {
+            "video_file": video_file,
+            "output_folder": output_folder,
+            "skip": skip,
+            "skip_beg": skip_beg,
+            "frames": frames,
+        }
+        for video_file in video_files
+    ]
+    pool = Pool(cpu_count() - 1)
+    pool.map(extract_frames_single_video_worker, extract_params)
+
+
+@cli.command(name="wel")
+def welcome():
+    click.echo("Welcome")
+
+
+if __name__ == "__main__":
+    cli()
